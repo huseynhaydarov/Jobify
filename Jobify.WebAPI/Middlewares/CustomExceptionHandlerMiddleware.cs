b3cd27b971b12d300c15d60c901306b7bfae0fa8
huseynhaydarov <huseynjonhaydarov@gmail.com>
2025-10-30 11:04:19 +0500

    added CustomExceptionHandlerMiddlewareExtensions

--- Jobify.WebAPI/Middlewares/CustomExceptionHandlerMiddleware.cs
+++ Jobify.WebAPI/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -76,3 +76,12 @@ public class CustomExceptionHandlerMiddleware(
         await httpContext.Response.WriteAsync(response);
     }
 }
+
+public static class CustomExceptionHandlerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCustomExceptionHandler(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+    }
+}
