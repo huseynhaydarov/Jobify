0a1458ca89c68b92f61fd0a732f4d3636c2b17f4
huseynhaydarov <huseynjonhaydarov@gmail.com>
2025-12-28 22:46:23 +0500

    initial tests setup

---
+++ Application.UnitTests/Common/TestAuthenticatedUser.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.UnitTests.Common;
+
+public sealed class TestAuthenticatedUser : IAuthenticatedUser
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public List<string>? Roles { get; }
+}
---
+++ Application.UnitTests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Application.UnitTests.Common;
+global using AutoFixture;
+global using AutoFixture.AutoNSubstitute;
+global using Jobify.Application.Common.Interfaces.Services;
+global using Jobify.Application.UseCases.Companies.Commands.CreateCompanies;
+global using Jobify.Infrastructure.Persistence.Data;
+global using Microsoft.EntityFrameworkCore;
+global using Shouldly;
