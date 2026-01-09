d071c862ef91c81edd6d26ef08474eb6bfcb1471
huseynhaydarov <huseynjonhaydarov@gmail.com>
2026-01-09 12:07:07 +0500

    removed old test files

--- Application.UnitTests/Common/TestAuthenticatedUser.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.UnitTests.Common;
-
-public sealed class TestAuthenticatedUser : IAuthenticatedUser
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public List<string>? Roles { get; }
-}
