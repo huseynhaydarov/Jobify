1e848f8daaf9ae79e82a636828541953dde8f55b
huseynhaydarov <huseynjonhaydarov@gmail.com>
2026-01-21 11:19:40 +0500

    added migrations

---
+++ src/Jobify.Infrastructure/Persistence/Migrations/20260121044127_AutidLogTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jobify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutidLogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EntityType = table.Column<string>(type: "text", nullable: false),
+                    Action = table.Column<string>(type: "text", nullable: false),
+                    ChangedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ChangedByUserEmail = table.Column<string>(type: "text", nullable: true),
+                    ChangedByType = table.Column<string>(type: "text", nullable: true),
+                    ChangedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Changes = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLogs");
+        }
+    }
+}
