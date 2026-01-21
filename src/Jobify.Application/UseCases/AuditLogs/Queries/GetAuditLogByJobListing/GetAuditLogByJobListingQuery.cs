f342574505d79c19de14a34e9d02c0d8b36c17cf
huseynhaydarov <huseynjonhaydarov@gmail.com>
2026-01-21 14:51:11 +0500

    added an endpoint for auditLog

---
+++ src/Jobify.Application/UseCases/AuditLogs/Queries/GetAuditLogByJobListing/GetAuditLogByJobListingQuery.cs
@@ -0,0 +1,6 @@
+﻿using Jobify.Application.UseCases.AuditLogs.Dtos;
+
+namespace Jobify.Application.UseCases.AuditLogs.Queries.GetAuditLogByJobListing;
+
+public record GetAuditLogByJobListingQuery(Guid Id, PagingParameters Parameters) :
+    IRequest<PaginatedResult<GetAuditLogByJobListingResponse>>;
---
+++ src/Jobify.Application/UseCases/AuditLogs/Queries/GetAuditLogByJobListing/GetAuditLogByJobListingQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Jobify.Application.UseCases.AuditLogs.Dtos;
+
+namespace Jobify.Application.UseCases.AuditLogs.Queries.GetAuditLogByJobListing;
+
+public class GetAuditLogByJobListingQueryHandler : BaseSetting, IRequestHandler<GetAuditLogByJobListingQuery,
+    PaginatedResult<GetAuditLogByJobListingResponse>>
+{
+    public GetAuditLogByJobListingQueryHandler(IApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<PaginatedResult<GetAuditLogByJobListingResponse>> Handle(GetAuditLogByJobListingQuery request,
+        CancellationToken cancellationToken)
+    {
+        var queryable = _dbContext.AuditLogs
+            .AsNoTracking()
+            .Where(a => a.EntityId == request.Id)
+            .OrderByDescending(c => c.ChangedAt)
+            .Select(j => new GetAuditLogByJobListingResponse
+            {
+                Id =  j.Id,
+                EntityType =  j.EntityType,
+                Action = j.Action.ToString(),
+                ChangedBy = j.ChangedBy,
+                ChangedByType = j.ChangedByType,
+                ChangedAt =  j.ChangedAt,
+                Changes =  j.Changes,
+                EntityId =  j.EntityId,
+            });
+
+         return await queryable.PaginatedListAsync(
+            request.Parameters.PageNumber,
+            request.Parameters.PageSize,
+            cancellationToken);
+    }
+}
